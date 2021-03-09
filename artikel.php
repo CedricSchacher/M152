2960a75b27333edb4aecb0e126fd97ee9870e697
cedric_schacher <cedi.sch@hotmail.ch>
2021-03-09 09:35:35 +0100

    Einzelne Seiten erstellt und überall HTML Struktur aufgebaut

---
+++ artikel.php
@@ -0,0 +1,29 @@
+
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>F1nfo</title>
+</head>
+<body>
+<div id="logo">
+
+</div>
+<h1>Artikeltitel</h1>
+<div id="bild">
+
+</div>
+<div id="artikel">
+
+</div>
+<div id="kommentar">
+
+</div>
+<div id="neuKommentar">
+    <input type="text" id="name">
+    <input type="text" id="kommentar">
+</div>
+
+</body>
+</html>
---
+++ hochladen.php
@@ -0,0 +1,34 @@
+
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>F1nfo</title>
+</head>
+<body>
+<div id="logo">
+
+</div>
+<form enctype="multipart/form-data" method="POST">
+<input type="text">
+<select name="license">
+    <option value="all-rights-reserved" selected>All rights reserved</option>
+    <option value="cc-by-nc-nd">CC BY-NC-ND</option>
+    <option value="cc-by-nd">CC BY-ND</option>
+    <option value="cc-by-nc-sa">CC BY-NC-SA</option>
+    <option value="cc-by-nc">CC BY-NC</option>
+    <option value="cc-by-sa">CC BY-SA</option>
+    <option value="cc-by">CC BY</option>
+    <option value="cc0">CC0 / Public Domain</option>
+</select>
+<input type="hidden" name="MAX_FILE_SIZE" value="2000000000">
+<input type="file" name="image">
+<input type="text">
+
+<input type="submit">
+</form>
+
+
+</body>
+</html>
