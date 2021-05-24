c759cb25a53ddb4ff19e457caa1da557997bb57d
cedric_schacher <cedi.sch@hotmail.ch>
2021-05-24 23:18:47 +0200

    !

---
+++ database.php
@@ -0,0 +1,30 @@
+
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "f1nfo";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+    }
+
+$sql = "SELECT * FROM Artikel";
+$result = $conn->query($sql);
+
+if($result->num_rows>0){
+    while ($row = $result->fetch_assoc()) {
+        echo "<div class='artikel'>";
+        echo "<H1>" . $row["Titel"]. "</H1><br>Text: " . $row["Text"];
+        echo "</div>";
+    }
+}
+    else{
+        echo "0 Results";
+    }
+
+?>
+
